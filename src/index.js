571b7202b64538ec5f20e1b8f6a49f0987c2527e
zkan <kan@prontomarketing.com>
2017-03-15 11:16:38 +0700

    Import style to use

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { add } from './utils.js'
+import './style.css'
 
 document.write(`3 + 5 = ${add(3, 5)}`)
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
       {
         test: /\.js$/,
         use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       }
     ]
   }
